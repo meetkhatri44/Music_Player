f7d3f7373d95c631c1cd7375ba6a75946610a4a5
meetkhatri44 <92850616+meetkhatri44@users.noreply.github.com>
2021-10-25 10:06:50 +0530

    Mood Refreshment

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+  {
+    name: "Industry Baby",
+    artist: "Lil nasx",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Good Life",
+    artist: "Kehlani ft G-Eazy",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "We Own It",
+    artist: "Wiz Khalifa",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Danza Kuduro",
+    artist: "Don Omar",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Whatever It Takes",
+    artist: "Imagine Dragons",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Hymn for the Weekend",
+    artist: "Coldplay",
+    img: "music-6",
+    src: "music-6"
+  },
+ 
+];
